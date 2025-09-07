0188f2781c8177f0cc77e9e3d76d21f37f02071e
Jonas Cardozo <cardozo.jonas@stthom.edu>
2025-09-07 16:24:07 -0500

    Simplified VeinMine, the veinmine is functional, needs security.

--- src/main/java/com/demomod/Demomod.java
+++ src/main/java/com/demomod/Demomod.java
@@ -1,26 +1,18 @@
 package com.demomod;
 
-import com.mojang.authlib.minecraft.client.MinecraftClient;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.mojang.authlib.yggdrasil.request.TelemetryEventsRequest.Event;
+
+import java.util.ArrayList;
 
 public class Demomod implements ModInitializer {
 	public static final String MOD_ID = "demomod";
@@ -46,8 +38,7 @@ public class Demomod implements ModInitializer {
 
         PlayerBlockBreakEvents.AFTER.register((world, player, pos, state, blockEntity) -> {
             if(!world.isClient()) {
-                System.out.println("hello");
-                System.out.println("hello");
+                System.out.println("67");
                 veinMine(world, pos, state, (ServerPlayerEntity) player);
 
             }
@@ -56,14 +47,15 @@ public class Demomod implements ModInitializer {
 
     private void veinMine(World world, BlockPos pos, BlockState state, ServerPlayerEntity player)
     {
-        System.out.println("hello");
-        var blocks = VeinMine.findBlocks(pos, world);
-        if(blocks != null) {
-            for (BlockPos b : blocks) {
-                if(!b.equals(pos)){
-                    world.breakBlock(b, true, player);
-                    System.out.println("block broken" + b.toString());
-                }
+        System.out.println("veinMine ran");
+        ArrayList<BlockPos> blocks = (ArrayList<BlockPos>) VeinMine.findBlockPath(world, pos, state, new ArrayList<BlockPos>());
+        System.out.println(blocks.toString());
+
+        for (BlockPos b : blocks) {
+            System.out.println("testing part 2");
+            if (!b.equals(pos)) {
+                world.breakBlock(b, true, player);
+                System.out.println("block broken" + b.toString());
             }
         }
     }
--- src/main/java/com/demomod/VeinMine.java
+++ src/main/java/com/demomod/VeinMine.java
@@ -10,70 +10,47 @@ import java.util.*;
 public class VeinMine {
 
 
-    public static List<BlockPos> findBlocks(BlockPos startingBlockPos, World world){
-        BlockState startState = world.getBlockState(startingBlockPos);
-        if(!world.isClient() && startState.getBlock() != Blocks.AIR){
-            return findBlockPath(world, startingBlockPos, startState);
-        }
-        return null;
-    }
+    public static List<BlockPos> findBlockPath(World world,
+                                               BlockPos startingBlockPos,
+                                               BlockState startState,
+                                               ArrayList<BlockPos> foundBlocks) { //DFS METHOD
+        if (foundBlocks.size() >= 50)
+            return foundBlocks;
+        int x = startingBlockPos.getX();
+        int y = startingBlockPos.getY();
+        int z = startingBlockPos.getZ();
 
-    public static List<BlockPos> findBlockPath(World world, BlockPos startingBlockPos, BlockState startState){ //DFS METHOD
-        Set<BlockPos> visited = new HashSet<>();
-        LinkedList<BlockPos> foundBlocks = new LinkedList<>();
-        Queue<BlockPos> queue = new LinkedList<>();
-
-        queue.add(startingBlockPos);
-        visited.add(startingBlockPos);
-
-        while(!queue.isEmpty()){
-            BlockPos current = queue.poll();
-            visited.add(current);
-
-            BlockState currentState = world.getBlockState(current);
-            System.out.println("Checking " + current + " state=" + currentState.getBlock());
-
-
-
-            if(currentState.getBlock().equals(startState.getBlock())){
-                foundBlocks.add(current);
-                findAllNeighbors(current, currentState, world, visited, foundBlocks, queue);
-            }
-
-        }
-
-        return foundBlocks;
-    }
-
-    public static void findAllNeighbors(BlockPos centerBlockPos,
-                                 BlockState centerBlockState,
-                                 World world,
-                                 Set<BlockPos> visited,
-                                 LinkedList<BlockPos> foundBlocks,
-                                 Queue<BlockPos> queue){ //DFS HELPER
-        ArrayList<BlockPos> neighbors = new ArrayList<>();
-
-        for(int dx = -1; dx <= 1; dx++){
-            for(int dy = -1; dy <= 1; dy++){
-                for(int dz = -1; dz <= 1; dz++){
-                    if(dx == 0 && dy == 0 && dz == 0){
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                for (int dz = -1; dz <= 1; dz++) {
+                    BlockPos current = new BlockPos(x + dx, y + dy, z + dz);
+                    BlockState state = world.getBlockState(current);
+                    if (!state.equals(startState))
                         continue;
-                    }
 
-                    BlockPos currentBlockPos = centerBlockPos.add(dx, dy, dz);
-                    BlockState currentBlockState = world.getBlockState(currentBlockPos);
-                    if(currentBlockState.getBlock().equals(centerBlockState.getBlock())) {
-                        if(!visited.contains(currentBlockPos)) {
-                            visited.add(currentBlockPos);
-                            foundBlocks.add(currentBlockPos);
-                            queue.add(currentBlockPos);
+                    boolean inList = false;
+                    for (BlockPos block : foundBlocks) {
+                        if (block.equals(current)) {
+
+                            inList = true;
+                            break;
+
 
                         }
+
                     }
+                    if (inList)
+                        continue;
+                    foundBlocks.add(current);
+                    findBlockPath(world, current, startState, foundBlocks);
 
                 }
+
             }
         }
+
+
+        return foundBlocks;
     }
 
 
