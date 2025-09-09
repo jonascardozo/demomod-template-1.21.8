93abeee3e0dbb019988d464845325fa331722b73
Jonas Cardozo <cardozo.jonas@stthom.edu>
2025-09-09 01:18:01 -0500

    Changed ConfigReader class into a Singleton as well as ConfigData. Deleted Deserializer class, wasn't necessary.

--- src/main/java/com/demomod/config/ConfigData.java
+++ src/main/java/com/demomod/config/ConfigData.java
@@ -1,21 +1,75 @@
 package com.demomod.config;
 
+import net.minecraft.text.BlockNbtDataSource;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.Set;
 
 public class ConfigData implements Serializable {
-    public static int dfs_limit;
-    public static String keybind;
-    public static int version;
-    public static ArrayList<String> tools;
+
+    private static volatile ConfigData instance;
+
+
+    public int dfs_limit = 100;
+    public String keybind = "undefined";
+    public String version = "1.0.0";
+
+
+    public Set<String> tools = Set.of(
+            "minecraft.diamond_pickaxe",
+            "minecraft.diamond_sword",
+            "minecraft.diamond_axe",
+            "minecraft.diamond_shovel",
+            "minecraft.diamond_hoe",
+
+            "minecraft.iron_pickaxe",
+            "minecraft.iron_sword",
+            "minecraft.iron_axe",
+            "minecraft.iron_shovel",
+            "minecraft.iron_hoe",
+
+            "minecraft.golden_pickaxe",
+            "minecraft.golden_sword",
+            "minecraft.golden_axe",
+            "minecraft.golden_shovel",
+            "minecraft.golden_hoe",
+
+            "minecraft.stone_pickaxe",
+            "minecraft.stone_sword",
+            "minecraft.stone_axe",
+            "minecraft.stone_shovel",
+            "minecraft.stone_hoe",
+
+            "minecraft.wooden_pickaxe",
+            "minecraft.wooden_sword",
+            "minecraft.wooden_axe",
+            "minecraft.wooden_shovel",
+            "minecraft.wooden_hoe",
+
+            "minecraft.shears"
+
+    );
+
+    public static ConfigData getInstance(){
+        ConfigData result = instance;
+        if(result == null){
+            synchronized (ConfigData.class){
+                result = instance;
+                if(result == null){
+                    instance = result = new ConfigData();
+                }
+            }
+        }
+        return result;
+    }
 
 
 
-    public String getKeybind() {return keybind;}
-    public int dfs_limit(){return dfs_limit;}
-    public int version(){return version;}
-    public ArrayList<String> tools(){return tools;}
+
+
+
+
+
 }
 
--- src/main/java/com/demomod/config/ConfigReader.java
+++ src/main/java/com/demomod/config/ConfigReader.java
@@ -1,35 +1,48 @@
 package com.demomod.config;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.stream.JsonReader;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import org.junit.jupiter.api.Assertions;
+import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ConfigReader {
-    static final Gson GSON = new Gson();
-    public static void of(Path path) {
+    private final Path vein_mine_config_path = Paths.get("vein_mine_config.json");
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private ConfigData config = new ConfigData();
 
-        Path file_path = Paths.get("config/vein_mine_config.json");
-        JsonReader reader;
-        {
-            try {
-                reader = new JsonReader(new FileReader(file_path.toString()));
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
+    private static ConfigReader instance;
+
+    public static ConfigReader getInstance(){
+        if(instance == null){
+            instance = new ConfigReader();
         }
+        return instance;
     }
 
-    public static void loadConfig(JsonReader reader){
-        GSON.fromJson(reader, ConfigData.class);
+//    public ConfigReader(){
+//        try{
+//            Assertions.assertTrue(Files.exists(vein_mine_config_path));
+//
+//
+//        }
+//        catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
+    public void loadConfig(){
+
+
     }
 
+    public void writeToConfig(){
 
 
-
+    }
 
 
 
